7b95380cb8ce624710865e83172f9808bbd3c605
miquel-toran-tramita <miquel@tramita.info>
2025-01-17 17:03:49 +0100

    todo

---
+++ src/components/CA.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  let ca = 'F9YaWZB2pvhf412XYX6Lq5eYF7dhV1ePWtjJesKgpump'
+  let show = false
+
+  const copy = () => {
+    navigator.clipboard.writeText(ca)
+    show = true
+
+    setTimeout(() => (show = false), 1000)
+  }
+</script>
+
+<style lang="scss">
+  @use 'src/sass/mixins.scss' as *;
+
+  button {
+    position: relative;
+    transition: 0.3s ease !important;
+    text-align: center;
+    width: 100%;
+    font-size: 24px;
+    background-image: url('/assets/banner.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 300px;
+
+    @include notDesktop {
+      font-size: 14px;
+      height: 200px;
+    }
+
+    &:hover {
+      box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.3);
+      transform: scale(0.9);
+    }
+  }
+</style>
+
+<button class="link" on:click={copy}> {show ? 'Copied to clipboard!' : ca} </button>
